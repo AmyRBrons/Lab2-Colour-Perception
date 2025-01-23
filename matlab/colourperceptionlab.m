9cc0da2ba08f629e0a36f86b65f1fce6153fd471
AmyBrons448 <97773647+AmyRBrons@users.noreply.github.com>
2025-01-23 15:49:16 +0000

    Adding

--- matlab/colourperceptionlab.m
+++ matlab/colourperceptionlab.m
@@ -5,15 +5,15 @@ imfinfo('peppers.png')
 
 %image shpw
 RGB = imread('peppers.png');  
-imshow(RGB)
+%imshow(RGB)
 
 %convert to grey
 I = rgb2gray(RGB);
 figure              % start a new figure window
-imshow(I)
+%imshow(I)
 
 %print pair side-by-side
-imshowpair(RGB, I, 'montage')
+%imshowpair(RGB, I, 'montage')
 title('Original colour image (left) grayscale image (right)')
 
 
@@ -21,17 +21,19 @@ title('Original colour image (left) grayscale image (right)')
 
 %split RGB channels
 [R,G,B] = imsplit(RGB);
-montage({R, G, B},'Size',[1 3])
+%montage({R, G, B},'Size',[1 3])
 
 %convert to HSV
 HSV = rgb2hsv(RGB);
 [H,S,V] = imsplit(HSV);
-montage({H,S,V}, 'Size', [1 3])
+%montage({H,S,V}, 'Size', [1 3])
 
 %___Task 13___
 
+
 %map xyz
 XYZ = rgb2xyz(RGB);
+%imshow(XYZ)
 [X,Y,Z] = imsplit(XYZ);
 montage({X,Y,Z}, 'Size', [1 3])
 
